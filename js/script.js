5d5364b322530ad2898e13e3b6dd524da4857acf
cwhyman <cwhyma@gmail.com>
2022-02-21 19:22:29 -0500

    Fixed 'add a new book' button behaviour (was not showing form on click)

--- js/script.js
+++ js/script.js
@@ -1,37 +1,41 @@
-"use strict";
+'use strict';
 
 let myLibrary = [];
 //define function to show form on button click
-const form  = document.getElementById('newBookForm');
+const form = document.getElementById('newBookForm');
+const showBookButton = document.getElementById('showBookButton');
+const newBookForm = document.getElementById('newBookForm');
 
-function showForm(){
-    var newBookForm = document.getElementById('newBookForm');
-    if (newBookForm.style.display === 'none') {
-        newBookForm.style.display = 'block';
-    } else {
-        newBookForm.style.display = 'none';
-    };
-};
+newBookForm.style.display = 'none';
+
+function showForm() {
+  if (newBookForm.style.display === 'none') {
+    newBookForm.style.display = 'block';
+    showBookButton.textContent = 'Hide form';
+  } else {
+    newBookForm.style.display = 'none';
+    showBookButton.textContent = 'Add a new book';
+  }
+}
 
 //define Book class
 class Book {
-    constructor(title, author, pages, readStatus) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.readStatus = readStatus;
-    }
+  constructor(title, author, pages, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+  }
 }
 
 // add function to toggle the read status of the Book prototype instance
 function toggleReadStatus(selectedBook) {
-    if (selectedBook.readStatus === 'Yes') {
-        selectedBook.readStatus = 'No'
-    } else {
-        selectedBook.readStatus = 'Yes'
-    };
-};
-
+  if (selectedBook.readStatus === 'Yes') {
+    selectedBook.readStatus = 'No';
+  } else {
+    selectedBook.readStatus = 'Yes';
+  }
+}
 
 //add test Books
 const newBook = new Book('testTitle', 'testAuthor', '999', 'Yes');
@@ -40,25 +44,25 @@ const newBook3 = new Book('testTitle3', 'testAuthor3', '7777', 'Yes');
 
 //create a function to add a new book object to the myLibrary array
 function addBookToLibrary(bookItem) {
-    return myLibrary.push(bookItem);
-};
+  return myLibrary.push(bookItem);
+}
 
 addBookToLibrary(newBook);
 addBookToLibrary(newBook2);
 addBookToLibrary(newBook3);
 
 //Create a function that loops through the array and displays each book
-function displayBooks() { 
-    var tBody = document.getElementById('table').getElementsByTagName('tbody')[0];
-    tBody.textContent = '';
-    for (let item of myLibrary) {
-        var row = tBody.insertRow(-1);
-        // set the id of the table row as the object index
-        row.dataset.key = myLibrary.indexOf(item).toString();
-        for (let key in item) {
-            var cell = row.insertCell(-1);
-            cell.textContent = item[key];
-        };
+function displayBooks() {
+  var tBody = document.getElementById('table').getElementsByTagName('tbody')[0];
+  tBody.textContent = '';
+  for (let item of myLibrary) {
+    var row = tBody.insertRow(-1);
+    // set the id of the table row as the object index
+    row.dataset.key = myLibrary.indexOf(item).toString();
+    for (let key in item) {
+      var cell = row.insertCell(-1);
+      cell.textContent = item[key];
+    }
     //add a read button to the end of each row
     var readToggle = document.createElement('INPUT');
     readToggle.setAttribute('type', 'checkbox');
@@ -74,52 +78,56 @@ function displayBooks() {
     deleteButton.setAttribute('id', row.dataset.key);
 
     row.appendChild(deleteButton);
-    };
-};
+  }
+}
 
 displayBooks();
 
 //add event listeners to all table buttons
 var table = document.getElementById('table');
 table.addEventListener('click', (event) => {
-    const isButton = event.target.nodeName === 'BUTTON';
+  const isButton = event.target.nodeName === 'BUTTON';
+  let rowId = event.target.id;
+  if (event.target.className === 'readToggle') {
+    toggleReadStatus(myLibrary[rowId]);
+    displayBooks();
+  } else if (event.target.className === 'deleteButton') {
     let rowId = event.target.id;
-    if (event.target.className === 'readToggle') {
-        toggleReadStatus(myLibrary[rowId]);
-        displayBooks();
-
-    } else if (event.target.className === 'deleteButton') {
-        let rowId = event.target.id
-        deleteBook(rowId);
-        console.log(myLibrary);
-    } else {
-        console.log(event.target.className);
-    };
+    deleteBook(rowId);
+    console.log(myLibrary);
+  } else {
+    console.log(event.target.className);
+  }
 });
 
 // function to add event listener to submit button, and add the new book to the library
 function submitForm() {
-    form.addEventListener('submit', (event) => {
+  form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    let formTitleValue = form.title.value
-    let formAuthorValue = form.author.value
-    let formPagesValue = form.pages.value
-    let formReadStatusValue = form.yesNo.value
-    
-    let newFormBook = new Book(formTitleValue, formAuthorValue, formPagesValue, formReadStatusValue);
+    let formTitleValue = form.title.value;
+    let formAuthorValue = form.author.value;
+    let formPagesValue = form.pages.value;
+    let formReadStatusValue = form.yesNo.value;
+
+    let newFormBook = new Book(
+      formTitleValue,
+      formAuthorValue,
+      formPagesValue,
+      formReadStatusValue
+    );
     addBookToLibrary(newFormBook);
     displayBooks();
-    });
-};
+  });
+}
 
 submitForm();
 
 //Create a function that allows the user to delete a book from the library
-function deleteBook(deleteIndex) { 
-    var tBody = document.getElementById('table').getElementsByTagName('tbody')[0];
-    var row = tBody.rows[deleteIndex];
-    var dataKey = row.dataset.key;
-    myLibrary.splice(dataKey, 1);
-    displayBooks();
-};
+function deleteBook(deleteIndex) {
+  var tBody = document.getElementById('table').getElementsByTagName('tbody')[0];
+  var row = tBody.rows[deleteIndex];
+  var dataKey = row.dataset.key;
+  myLibrary.splice(dataKey, 1);
+  displayBooks();
+}
